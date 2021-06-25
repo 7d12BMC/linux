939bfee0877fa095b090b3ba0d4f074fc9e0d807
allegro0132 <34514398+allegro0132@users.noreply.github.com>
2021-06-25 17:30:11 +0800

    Delete drm_pci.h

--- drivers/gpu/drm/ast/ast_drv.c
+++ drivers/gpu/drm/ast/ast_drv.c
@@ -33,7 +33,7 @@
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_gem_vram_helper.h>
-#include <drm/drm_pci.h>
+
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_vram_mm_helper.h>
 
